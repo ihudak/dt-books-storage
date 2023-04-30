d7d067d220da716fd76ae12bbb0684f96b0be347
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-04-30 14:40:14 +0200

    add count to version

--- src/main/java/com/dynatrace/storage/controller/VersionController.java
+++ src/main/java/com/dynatrace/storage/controller/VersionController.java
@@ -1,6 +1,8 @@
 package com.dynatrace.storage.controller;
 
 import com.dynatrace.storage.model.Version;
+import com.dynatrace.storage.repository.StorageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/version")
 public class VersionController {
+    @Autowired
+    private StorageRepository storageRepository;
     @Value("${service.version}")
     private String svcVer;
     @Value("${service.date}")
@@ -17,6 +21,6 @@ public class VersionController {
 
     @GetMapping("")
     public Version getVersion() {
-        return new Version("storage", svcVer, svcDate, "OK", "Healthy");
+        return new Version("storage", svcVer, svcDate, "OK", "Count: " + storageRepository.count());
     }
 }
